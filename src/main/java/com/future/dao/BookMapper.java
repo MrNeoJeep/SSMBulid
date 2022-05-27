3bd2a009ea1b762e1861b8c2cdb00350c2c38e13
MrNeoJeep <1656153777@qq.com>
2022-05-27 22:58:33 +0800

    update

---
+++ src/main/java/com/future/dao/BookMapper.java
@@ -0,0 +1,19 @@
+package com.future.dao;
+
+import com.future.pojo.Books;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BookMapper {
+    //增加一本书
+    int addBook(Books books);
+    //删除一本书
+    int deleteBookById(@Param("id")int id);
+    //更新一本书
+    int updateBook(Books books);
+    //查询一本书
+    Books queryBookById(@Param("id")int id);
+    //查询所有书
+    List<Books> queryAllBook();
+}
