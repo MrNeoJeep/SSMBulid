e9ce544d2e9ed84b79c4ee68ca27dc69fd918c89
MrNeoJeep <1656153777@qq.com>
2022-05-29 11:21:29 +0800

    update

---
+++ src/main/java/com/future/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.future.controller;
+
+import com.future.pojo.Books;
+import com.future.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+    //controller调service层
+    @Autowired
+    @Qualifier("BookServiceImpl")
+    private BookService bookService;
+
+    //查询所有书籍，返回到书籍展示页面
+    @RequestMapping("/allBook")
+    public String list(Model model){
+        List<Books> books = bookService.queryAllBook();
+        model.addAttribute("list",books);
+        return "allBook";
+    }
+}
--- web/index.jsp
+++ web/index.jsp
@@ -1,16 +1,9 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: 风
-  Date: 2022/5/29
-  Time: 10:31
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>首页</title>
   </head>
   <body>
-  $END$
+  <a href="/book/allBook" style="text-align: center">进入书籍展示页面</a>
   </body>
 </html>
